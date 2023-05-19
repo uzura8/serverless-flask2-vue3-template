d2d66567c75373fd28d94221e128464f5e417fb6
Shingo Yamada <uzuranoie@gmail.com>
2023-05-19 22:08:57 +0900

    Implemented to save game number

--- serverless/app/models/dynamodb/event.py
+++ serverless/app/models/dynamodb/event.py
@@ -2,6 +2,7 @@
 
 from app.models.dynamodb.base import Base
 
+
 class Event(Base):
     """DynamoDB Model class for `event` table."""
 
@@ -21,6 +22,7 @@ class Event(Base):
         'joinedCount',
         'windType',
         'temperature',
+        'gameNum'
     ]
 
     response_attrs = public_attrs + [
@@ -32,3 +34,21 @@ class Event(Base):
     all_attrs = public_attrs + private_attrs
 
     allowed_event_types = []
+
+    @classmethod
+    def increament_game_num(self, event_id, is_check_exist=False):
+        """Increament game number."""
+
+        if is_check_exist:
+            item = self.get_one_by_pkey('eventId', event_id)
+            if not item:
+                raise Exception('Event not found')
+
+        table = self.get_table()
+        table.update_item(
+            Key={'eventId': event_id},
+            UpdateExpression='ADD gameNum :incr',
+            ExpressionAttributeValues={':incr': 1}
+        )
+        item = self.get_one_by_pkey('eventId', event_id)
+        return item['gameNum']
--- serverless/app/models/dynamodb/game.py
+++ serverless/app/models/dynamodb/game.py
@@ -1,6 +1,7 @@
 """This module provides an example import from the `app.models.dynamodb.base` module."""
 
 from app.models.dynamodb.base import Base
+from app.models.dynamodb.event import Event
 
 
 class Game(Base):
@@ -31,3 +32,12 @@ class Game(Base):
     all_attrs = public_attrs + private_attrs
 
     allowed_event_types = []
+
+    @classmethod
+    def create(self, vals, uuid_name=None):
+        """Create a new game."""
+        # TODO: implement transaction
+        game_num = Event.increament_game_num(vals['eventId'])
+        vals['gameNum'] = game_num
+
+        return super().create(vals, uuid_name)
