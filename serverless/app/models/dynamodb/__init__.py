7d66a5f457108d9ba99dc9fcd8d141763dd009ec
Shingo Yamada <uzuranoie@gmail.com>
2023-07-31 10:13:40 +0900

    Removed unused table definitions

--- serverless/app/models/dynamodb/__init__.py
+++ serverless/app/models/dynamodb/__init__.py
@@ -1,30 +1,14 @@
 from .base import Base, ModelInvalidParamsException
 from .site_config import SiteConfig
-# from .service import Service
-# from .service_config import ServiceConfig
 # from .admin_user_config import AdminUserConfig
-# from .post import Post
-# from .post_group import PostGroup
-# from .comment import Comment
-# from .comment_count import CommentCount
 from .category import Category
-# from .tag import Tag
-# from .post_tag import PostTag
 # from .file import File
 
 __all__ = [
     'Base',
     'ModelInvalidParamsException',
     'SiteConfig',
-    # 'Service',
-    # 'ServiceConfig',
     # 'AdminUserConfig',
-    # 'Post',
-    # 'PostGroup',
-    # 'Comment',
-    # 'CommentCount',
     'Category',
-    # 'Tag',
-    # 'PostTag',
     # 'File',
 ]
